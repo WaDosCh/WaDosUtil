4900a10a2e5e7a856b658885d55f16437b42a13d
judos <judos@gmx.ch>
2015-08-31 23:09:59 +0200

    added udp, upnp network example files
    a simple portforwarding example is also included
    simple to use files to demonstrate file transfers

---
+++ judosTest/ch/judos/examples/udp_portforwarding/TestGenericUpnpForwarding.java
@@ -0,0 +1,39 @@
+package ch.judos.examples.udp_portforwarding;
+
+import ch.judos.generic.network.udp.Udp4Forwarded;
+import ch.judos.generic.network.udp.UdpLib;
+
+/**
+ * @since 31.08.2015
+ * @author Julian Schelker
+ */
+public class TestGenericUpnpForwarding {
+	public static void main(String[] args) throws Exception {
+		new TestGenericUpnpForwarding();
+	}
+
+	public TestGenericUpnpForwarding() throws Exception {
+		Udp4Forwarded lib = UdpLib.createForwarded();
+		System.out.println("Created udp socket on port: " + lib.getLocalPort());
+		System.out.println("Mapped with portforwarding to external port: "
+			+ lib.getExternalPort());
+
+		synchronized (this) {
+			this.wait(1000);
+		}
+		lib.dispose();
+
+		System.out.println();
+		System.out.println("Trying to forward specific port:");
+
+		lib = UdpLib.createForwarded(50001);
+		System.out.println("Created udp socket on port: " + lib.getLocalPort());
+		System.out.println("Mapped with portforwarding to external port: "
+			+ lib.getExternalPort());
+
+		synchronized (this) {
+			this.wait(1000);
+		}
+		lib.dispose();
+	}
+}
