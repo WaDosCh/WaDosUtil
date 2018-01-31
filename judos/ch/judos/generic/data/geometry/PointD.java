91ffe55311a9452f2e2c5487b21d3a2edee1901d
judos <judos@gmx.ch>
2018-01-31 21:59:54 +0100

    add rotate to PointD

--- judos/ch/judos/generic/data/geometry/PointD.java
+++ judos/ch/judos/generic/data/geometry/PointD.java
@@ -10,4 +10,14 @@ public class PointD extends Point2D.Double {
 
 	private static final long serialVersionUID = -3551698231506636754L;
 
+	public void rotate(double angleRadian) {
+		double rx = (this.x * Math.cos(angleRadian)) - (this.y * Math.sin(angleRadian));
+		double ry = (this.x * Math.sin(angleRadian)) + (this.y * Math.cos(angleRadian));
+		this.x = rx;
+		this.y = ry;
+	}
+
+	public void rotate(Angle angle) {
+		rotate(angle.getRadian());
+	}
 }
